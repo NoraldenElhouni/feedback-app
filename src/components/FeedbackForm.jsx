7ddf8e9b52ee63dc0eff963e0510059ed647d61a
Nouralden Elhouni <noralden.honi@gmail.com>
2023-09-06 23:02:15 +0200

    animated feed

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import React from "react";
+import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 
-function FeedbackForm() {
+function FeedbackForm({ handelAdd }) {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
@@ -23,11 +25,24 @@ function FeedbackForm() {
     setText(e.target.value);
   };
 
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+      handelAdd(newFeedback);
+
+      setText("");
+    }
+  };
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handelSubmit}>
         <h2>How would you rate your service with us</h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handelTextChange}
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import PropType from "prop-types";
 import Feedbackitem from "./FeedbackItem";
 import React from "react";
@@ -9,12 +10,34 @@ function FeedbackList({ feedback, handelDelete }) {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <Feedbackitem key={item.id} item={item} handelDelete={handelDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Feedbackitem
+              key={item.id}
+              item={item}
+              handelDelete={handelDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
+
+//   return (
+//     <div className="feedback-list">
+//       {feedback.map((item) => (
+//         <Feedbackitem key={item.id} item={item} handelDelete={handelDelete} />
+//       ))}
+//     </div>
+//   );
+// }
 
 FeedbackList.PropType = {
   feedback: PropType.arrayOf(
